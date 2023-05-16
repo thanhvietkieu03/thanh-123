952ad99f5a33a7846e1ad23ffc8fc5bf012687ed
unknown <thanhvietkieu03@gmail.com>
2023-05-16 12:16:24 +0700

    Add model validation and association

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'home#index'
-  root 'controller#action'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
